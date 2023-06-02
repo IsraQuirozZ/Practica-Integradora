5a46fd07dcd4f28015c781c36c192717220be701
Israel <isra_quirozz@hotmail.com>
2023-06-02 02:56:40 +0200

    Complete users endpoints

--- src/router/users.js
+++ src/router/users.js
@@ -1,14 +1,14 @@
 import { Router } from "express";
-import Movie from "../models/Movie.js";
+import User from "../models/User.js";
 
 let router = Router();
 
 router.post("/", async (req, res, next) => {
   try {
-    let movie = await Movie.create(req.body);
+    let user = await User.create(req.body);
     return res.status(201).json({
       success: true,
-      message: `movie id=${movie._id} created`,
+      response: `user: ${user.name} created with id: "${user._id}"`,
     });
   } catch (error) {
     next(error);
@@ -17,17 +17,12 @@ router.post("/", async (req, res, next) => {
 
 router.get("/", async (req, res, next) => {
   try {
-    let movies = await Movie.find();
-    if (movies) {
+    let users = await User.find();
+    if (users) {
       return res.status(200).json({
         success: true,
-        quantity: movies.length,
-        response: movies,
-      });
-    } else {
-      return res.status(404).json({
-        success: false,
-        response: "not found movies",
+        quantity: users.length,
+        response: users,
       });
     }
   } catch (error) {
@@ -39,16 +34,22 @@ router.put("/:id", async (req, res, next) => {
   try {
     let id = req.params.id;
     let data = req.body;
-    let movie = await Movie.findByIdAndUpdate(id, data, { new: true });
-    if (movie) {
+    if (Object.entries(data).length === 0) {
+      return res.status(400).json({
+        success: false,
+        response: `check your data`,
+      });
+    }
+    let user = await User.findByIdAndUpdate(id, data, { new: true });
+    if (user) {
       return res.status(200).json({
         success: true,
-        response: movie,
+        response: user,
       });
     } else {
       return res.status(404).json({
         success: false,
-        response: "not found movie",
+        response: `not found user with id: ${id}`,
       });
     }
   } catch (error) {
@@ -59,16 +60,16 @@ router.put("/:id", async (req, res, next) => {
 router.delete("/:id", async (req, res, next) => {
   try {
     let id = req.params.id;
-    let movie = await Movie.findByIdAndDelete(id);
-    if (movie) {
+    let user = await User.findByIdAndDelete(id);
+    if (user) {
       return res.status(200).json({
         success: true,
-        response: `movie: "${movie.title}" deleted`,
+        response: `User "${user._id}": ${user.name} ${user.last_name} deleted`,
       });
     } else {
       return res.status(404).json({
         success: false,
-        response: "not found movie",
+        response: "not found user",
       });
     }
   } catch (error) {
