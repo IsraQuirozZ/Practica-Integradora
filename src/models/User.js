1e856079b70762d64256b2c92669dd46876b7bc5
Israel <isra_quirozz@hotmail.com>
2023-06-07 01:54:46 +0200

    Practica integradora 2

--- src/models/User.js
+++ src/models/User.js
@@ -5,7 +5,7 @@ let collection = "users";
 let schema = new Schema({
   name: { type: String, required: true },
   last_name: { type: String, required: true },
-  age: { type: Number, required: true },
+  age: { type: Number, index: true }, //ejemplo para estadísticos que dependan de rango etario
 });
 
 let User = model(collection, schema);
